ff4c92ad7bf32d6483ed5d7927557e5293230c9f
Hikasa Akio <3445djwgodjw@sunmoon.ac.kr>
2022-12-13 13:21:35 +0900

    Initial sharing of project

---
+++ Borrower.java
@@ -0,0 +1,37 @@
+
+/**
+ * 여기에 Borrower 클래스 설명을 작성하십시오.
+ * 
+ * @author (작성자 이름) 
+ * @version (버전번호나 날짜)
+ */
+public class Borrower
+{
+    //instance variable
+    private String name; 
+    static public String name;
+    /*
+     * Constructor for Borrower Class
+     */
+    public Borrower(){
+    
+    }
+    /*
+     * Methods and Comments
+     * attachBook(Object)→ <identifier>expected
+     */
+    public void attachBook(Object){
+
+    }
+    /*
+     * detachBook() return:void
+     */
+    public void detachBook(){
+    } 
+        /*
+     * detachBook() return:void
+     */
+    public loanableBorrower(){
+        return boolean
+    }
+}
